716c7f2ca467744bce1faa7a23ee790b566d666f
Mahdi <hassanim430@gmail.com>
2023-04-14 16:19:49 +0430

    adding new typescript component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Greet />
     </div>
   );
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+const Greet = (props: Props) => {
+    const {} = props
+
+    return (
+        <div>
+            <h1>Hello from Greet</h1>
+        </div>
+    )
+}
+
+export default Greet;
